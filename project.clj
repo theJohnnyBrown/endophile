3784eacc00346de9b34d702778dd14b855a16391
Johnny Brown <jb@stafftacular.com>
2013-02-21 17:08:57 -0300

    version and description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject endophile "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject endophile "0.1.0"
+  :description "See README.md"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
