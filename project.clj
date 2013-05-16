00866cdc4cc2721fea3e3acd906bf9eeb5cb71de
Johnny Brown <mealplans@rippedathlete.com>
2013-05-16 16:36:30 -0400

    two more tests down

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.pegdown/pegdown "1.2.0"]
+                 [net.sf.jtidy/jtidy "r938"]
                  [enlive "1.1.1"]])
--- test/endophile/core_test.clj
+++ test/endophile/core_test.clj
@@ -2,6 +2,8 @@
   (:use clojure.test
         clojure.java.io
         endophile.core)
+  (:import [java.io StringWriter StringReader]
+           [org.w3c.tidy Tidy])
   (:require [net.cgrand.enlive-html :as html]
             [clojure.string :as str]))
 
@@ -11,14 +13,21 @@
    (fn [ff] (= (subs (.getName ff) (- (count (.getName ff)) 4)) "text"))
    files))
 
-(defn uglify [s] (str/replace s #"[\n]+\s*" ""))
-
+(defn tidy [untidy]
+  (let [w (StringWriter.)]
+    (doto (Tidy.)
+      (.setTabsize 4)
+      (.setPrintBodyOnly true)
+      (.setShowWarnings false)
+      (.setQuiet true)
+      (.parse (StringReader. untidy) w))
+    (str/replace (.toString w) "\r\n" "\n")))
 
 (deftest a-test
   (doseq [md-file (-> test-files-dir file .listFiles markdown-files)]
    (is
-    (= (uglify (apply str (html/emit* (to-clj (mp (slurp md-file))))))
-       (uglify (slurp (str
+    (= (tidy (apply str (html/emit* (to-clj (mp (slurp md-file))))))
+       (tidy (slurp (str
                        test-files-dir
                        (str/replace (.getName md-file)
                                     ".text" ".html")))))
