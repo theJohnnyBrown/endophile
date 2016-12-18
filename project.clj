a86a002ba2780323198e67da79242e39b5fd9467
Juho Teperi <juho.teperi@iki.fi>
2016-12-18 21:36:30 +0200

    Run Travis tests on Clojure 1.6-1.9

--- project.clj
+++ project.clj
@@ -6,4 +6,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.pegdown/pegdown "1.6.0"]
                  [enlive "1.1.6"]
-                 [hiccup "1.0.5"]])
+                 [hiccup "1.0.5"]]
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.9:dev,1.7:dev,1.6"]})
