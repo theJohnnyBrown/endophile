774d5decbf89f3c64a87e2bfa3a0f587e6c2ab7b
Juho Teperi <juho.teperi@iki.fi>
2016-12-18 21:26:29 +0200

    Update Clojure dependency, update enlive

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/theJohnnyBrown/endophile/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.pegdown/pegdown "1.6.0"]
-                 [enlive "1.1.5"]
+                 [enlive "1.1.6"]
                  [hiccup "1.0.5"]])
